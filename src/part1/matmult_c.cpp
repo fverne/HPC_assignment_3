a5c221ff1cc49c316372e9cff194d7f4312c61a1
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-01-17 23:03:15 +0100

    implement matmult_mkn_offload()

--- src/part1/matmult_c.cpp
+++ src/part1/matmult_c.cpp
@@ -3,6 +3,14 @@ extern "C" {
     #include <omp.h>
     #include <stdio.h>
 
+    #ifndef _TEAMS
+    #define _TEAMS 16384
+    #endif
+
+    #ifndef _THREADS
+    #define _THREADS 16
+    #endif
+
     void 
     matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C) {
         // double t1, t2;
@@ -35,4 +43,32 @@ extern "C" {
         *B, n, beta, *C, n);
     }
 
+    // offload
+    void 
+    matmult_mkn_offload(int m, int n, int k, double **A, double **B, double**C) {
+        // double t1, t2;
+        // t1 = omp_get_wtime();
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                C[i][j] = 0;
+
+        #pragma omp target teams distribute parallel for \
+        map (to: A[0:m][0:k], B[0:k][0:n], m,k,n) map(tofrom: C[0:m][0:n]) \
+        num_teams(_TEAMS) thread_limit(_THREADS) collapse(2)
+        for(int i=0;i<m;i++){
+            for(int l=0;l<k;l++){
+                for(int j=0;j<n;j++){
+                    #pragma omp atomic
+                    C[i][j] += A[i][l]*B[l][j];
+                }
+            }
+        }
+        // t2 = omp_get_wtime();
+        // printf("Time with transfer: %f\n", 1e3*(t2-t1));
+    }
+
+    void 
+    matmult_mnk_offload(int m, int n, int k, double **A, double **B, double**C) {
+
+    }
 }
--- src/part1/matmult_c.h
+++ src/part1/matmult_c.h
@@ -5,4 +5,6 @@ void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C);
 
 void matmult_lib(int m,int n,int k,double **A,double **B,double **C);
 
+void matmult_mkn_offload(int m, int n, int k, double **A, double **B, double**C);
+
 #endif
