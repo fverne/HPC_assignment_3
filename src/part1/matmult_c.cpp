dfbb05df507dff6cbfc04a7687d85e9853cffc2a
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-01-17 23:02:53 +0100

    update mkn_omp()

--- src/part1/matmult_c.cpp
+++ src/part1/matmult_c.cpp
@@ -5,20 +5,23 @@ extern "C" {
 
     void 
     matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C) {
+        // double t1, t2;
+        // t1 = omp_get_wtime();
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
                 C[i][j] = 0;
 
-        #pragma omp parallel for default(none) shared(m, n, k, A, B, C)
+        #pragma omp parallel shared(m, n, k, A, B, C) for
         for(int i = 0; i < m; i++) {
             for(int l = 0; l < k; l++) {
-                double sum = 0;
                 for(int j = 0; j < n; j++) {
-                    sum += A[i][l] * B[l][j];
+                    #pragma omp atomic
+                    C[i][j] += A[i][l] * B[l][j];
                 }
-                C[i][l] = sum;
             }
         }
+        // t2 = omp_get_wtime();
+        // printf("Time: %lf\n", (t2-t1)*1e3);
     }
 
     void
